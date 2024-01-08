c113ca49bf4eb21b1a6c10545ce6942727eee3ac
Wu Feiteng <wufeiteng@bytedance.com>
2024-01-08 17:53:47 +0800

    disable raw mode at exit and turn off canonical mode

--- kilo.c
+++ kilo.c
@@ -1,10 +1,19 @@
 #include <termios.h>
+#include <stdlib.h>
 #include <unistd.h>
 
+struct termios orig_termios;
+void disableRawMode() {
+  tcgetattr(STDIN_FILENO, &orig_termios);
+}
+
 void enableRawMode() {
-  struct termios raw;
-  tcgetattr(STDIN_FILENO, &raw);
-  raw.c_lflag &= ~(ECHO);
+  tcgetattr(STDIN_FILENO, &orig_termios);
+  atexit(disableRawMode);
+
+  struct termios raw = orig_termios;
+  raw.c_lflag &= ~(ECHO | ICANON);
+
   tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
 
